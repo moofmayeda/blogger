ffbce607bd5bf128bf976e762874d72e629fd67b
moof mayeda <moofmayeda@yahoo.com>
2014-10-01 11:03:25 -0700

    pop up messages about Dracula's real name and secret messages

---
+++ controllers/about.js
@@ -0,0 +1,7 @@
+Blogger.AboutController = Ember.Controller.extend({
+  actions: {
+    showRealName: function() {
+      alert("Dracula is fictional, but was inspired by the 15th-century Romanian general and Wallachian Prince Vlad III the Impaler.")
+    }
+  }
+});
