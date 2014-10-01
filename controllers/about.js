f2599e41737c653f4472ac63d6e12d7856e6e9e2
moof mayeda <moofmayeda@yahoo.com>
2014-10-01 11:09:15 -0700

    show/hide a pic of Dracula

--- controllers/about.js
+++ controllers/about.js
@@ -1,7 +1,14 @@
 Blogger.AboutController = Ember.Controller.extend({
+  isPictureShowing: false,
   actions: {
     showRealName: function() {
       alert("Dracula is fictional, but was inspired by the 15th-century Romanian general and Wallachian Prince Vlad III the Impaler.")
+    },
+    showPicture: function() {
+      this.set('isPictureShowing', true);
+    },
+    hidePicture: function() {
+      this.set('isPictureShowing', false);
     }
   }
 });
